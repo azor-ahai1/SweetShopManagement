1b8724516aa618c687e26ec51a30e57f94c66455
Aashish Shukla <aks1092002@gmail.com>
2025-09-26 16:03:36 +0530

    chors: store setup in frontend

---
+++ frontend/src/store/authSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    user: null, 
+    userAuth: false,
+    token: null
+  },
+
+  reducers: {
+    login(state, action) {
+      state.user = action.payload.user;
+      state.userAuth = true;
+      state.token = action.payload.accessToken;
+      state.isAdmin = action.payload.user?.role === 'admin'; 
+      
+      localStorage.setItem('user', JSON.stringify(action.payload.user));
+      localStorage.setItem('token', action.payload.accessToken);
+    },
+    logout(state) {
+      state.user = null; 
+      state.userAuth = false;
+      state.token = null;
+      state.isAdmin = false;  
+
+      localStorage.removeItem('user');
+      localStorage.removeItem('token');
+    }
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+
+export const selectUser  = (state) => state.auth.user; 
+export const selectUserAuth = (state) => state.auth.userAuth;
+export const selectToken = (state) => state.auth.token;
+export const selectIsAdmin = (state) => state.auth.isAdmin;
+
+export default authSlice.reducer;
