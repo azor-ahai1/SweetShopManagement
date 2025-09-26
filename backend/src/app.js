509b6c03f13bdb4c583480a6a6ee4194255cc412
Aashish Shukla <aks1092002@gmail.com>
2025-09-26 12:37:19 +0530

    chore: added middlewares and cloudinary service

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,7 +30,7 @@ app.use(cookieParser())
 
 // ----------------------------------------------------------------------------------
 
-import userRouter from "./routes/user.routes.js";
+import userRouter from "./routes/user.route.js";
 
 app.use("/api/v1/users", userRouter);
 
