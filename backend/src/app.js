dc8ebf8bb0427400a3c2eba5652ccdcfdfbea476
Aashish Shukla <aks1092002@gmail.com>
2025-09-26 09:38:00 +0530

    feat: implemented basic routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import { ApiResponse } from './utils/ApiResponse';
 
 const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json(new ApiResponse(200, null, 'Welcome to Sweet Shop API'));
+});
 
 export { app };
