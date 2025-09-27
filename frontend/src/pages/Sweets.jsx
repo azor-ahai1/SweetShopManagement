b78fe6380f5d524dbd295abc0183fb7063613185
Aashish Shukla <aks1092002@gmail.com>
2025-09-27 12:47:13 +0530

    fix: minor bug fixed

--- frontend/src/pages/Sweets.jsx
+++ frontend/src/pages/Sweets.jsx
@@ -238,7 +238,7 @@ function Sweets() {
                     </div>
                 </div>
             ) : (
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
                     {sweets.map(sweet => (
                         <SweetCard 
                             key={sweet._id} 
